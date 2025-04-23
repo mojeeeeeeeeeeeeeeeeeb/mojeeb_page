a1ee4ea5804c610986d090dcf790373a55526cb0
mojeeeeeeeeeeeeeeeeeb <abdokingr6@gmail.com>
2025-04-23 21:18:00 +0200

    Add files via upload

---
+++ a.cmd
@@ -0,0 +1,7 @@
+echo "# mojeeb_page" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mojeeeeeeeeeeeeeeeeeb/mojeeb_page.git
+git push -u origin main
